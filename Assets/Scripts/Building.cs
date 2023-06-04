a3df86610e948eea67a56f00eaf1b47bf0618673
Maxwel Santana <maxwelsant96@icloud.com>
2023-06-03 23:44:47 -0400

    First commit

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+    public enum BuildingState { staying, replacing, updating, firstStart, destroying };
+    [HideInInspector]
+    public BuildingState buildingState;
+    
+    //Variables
+    [HideInInspector]
+    public bool notToMove;
+    [HideInInspector]
+    public bool mouseDrag;
+    [HideInInspector]
+    Vector3 currentPosition;
+    private float zCordinateOfMouse;
+    private Vector3 mouseOffset;
+
+    [Header("Scripts")]
+    private GameManager gameManager;
+    private SpriteRenderer spriteRenderer;
+
+    [System.Serializable]
+    public class Update
+    {
+        public Sprite updateLevelSprite;
+    }
+    public Update[] updates;
+    public int nextUpdate;
+
+
+    void Start()
+    {
+        Debug.Log("STARTED");
+        //Set currentPosition to current position of building 
+        currentPosition = transform.position;
+
+        //Get the scripts
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    /*
+    void FixedUpdate()
+    {
+        if (buildingState == BuildingState.staying)
+        {
+            yesNoPanel.SetActive(false);
+            arrows.SetActive(false);
+            updatePanel.SetActive(false);
+            slayers.number = -0.2f;
+            tile.stayingState = true;
+        }
+        if (buildingState == BuildingState.replacing)
+        {
+            yesNoPanel.SetActive(true);
+            arrows.SetActive(true);
+            updatePanel.SetActive(false);
+            slayers.number = 0.2f;
+            tile.stayingState = false;
+
+            if (tile.isOcupied == true)
+            {
+                yesButton.interactable = false;
+            }
+            else
+            {
+                yesButton.interactable = true;
+            }
+        }
+        if (buildingState == BuildingState.updating)
+        {
+            yesNoPanel.SetActive(false);
+            arrows.SetActive(false);
+            if (nextUpdate + 1 != updates.Length)
+            {
+                updatePanel.SetActive(true);
+            }
+            else
+            {
+                updatePanel.SetActive(false);
+            }
+
+            slayers.number = 0.2f;
+            tile.stayingState = true;
+        }
+        if (buildingState == BuildingState.firstStart)
+        {
+            yesNoPanel.SetActive(true);
+            arrows.SetActive(true);
+            updatePanel.SetActive(false);
+            slayers.number = 0.2f;
+            tile.stayingState = false;
+        }
+        if (buildingState == BuildingState.destroying)
+        {
+            yesNoPanel.SetActive(true);
+            arrows.SetActive(false);
+            updatePanel.SetActive(false);
+            slayers.number = 0.2f;
+            tile.stayingState = true;
+        }
+        if (gameManager.selectedBuilding != gameObject)
+        {
+            buildingState = BuildingState.staying;
+        }
+
+        spriteRenderer.sprite = updates[nextUpdate].updateLevelSprite;
+
+    }
+    */
+
+    void OnMouseDown()
+    {
+        zCordinateOfMouse = Camera.main.WorldToScreenPoint(transform.position).z;
+        mouseOffset = transform.position - mouseWorldPos();
+        Debug.Log("gameManager.replaceMode: ");
+        if (gameManager.buildingSelected == false)
+        {
+            if (gameManager.replaceMode == true)
+            {
+                buildingState = BuildingState.replacing;
+                gameManager.buildingSelected = true;
+                mouseDrag = true;
+            }
+            else if (gameManager.destroyMode == true)
+            {
+                buildingState = BuildingState.destroying;
+                gameManager.buildingSelected = true;
+                mouseDrag = false;
+            }
+            else
+            {
+                if (nextUpdate + 1 != updates.Length)
+                {
+                    buildingState = BuildingState.updating;
+                    gameManager.buildingSelected = true;
+                }
+                mouseDrag = false;
+            }
+            gameManager.selectedBuilding = gameObject;
+
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("OnMouseDrag: " + mouseDrag);
+        if (mouseDrag == true)
+        {
+            transform.position = mouseWorldPos() + mouseOffset;
+        }
+    }
+
+    private Vector3 mouseWorldPos()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = zCordinateOfMouse;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Arrays")]
+    public GameObject[] structureArray;
+
+    [Header("Game Objects")]
+    public GameObject mainPanel;
+    public GameObject shop;
+    public GameObject spawnPointOfBuildings;
+    public GameObject gameOver;
+    public GameObject youWinLabel;
+    public GameObject youLostLabel;
+
+    //Variables
+    [HideInInspector]
+    public bool buildingSelected;
+    [HideInInspector]
+    public int buildingNumber;
+    [HideInInspector]
+    public bool replaceMode;
+    [HideInInspector]
+    public bool destroyMode;
+
+    [HideInInspector]
+    public GameObject selectedBuilding;
+
+    public int Wave { get; set; }
+
+    public Text goldLabel;
+    private int gold;
+    public int Gold
+    {
+        get
+        {
+            return gold;
+        }
+        set
+        {
+            gold = value;
+            goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+        }
+    }
+
+    public Text healthLabel;
+    private int health;
+    public int Health 
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            healthLabel.GetComponent<Text>().text = "HEALTH: " + health;
+        }
+    }
+
+    void Start()
+    {
+        mainPanel.SetActive(true);
+        shop.SetActive(false);
+        gameOver.SetActive(false);
+        youLostLabel.SetActive(false);
+        youWinLabel.SetActive(false);
+
+        Gold = 1000;
+        Health = 5;
+        Wave = 0;
+    }
+
+
+    //Buttons
+
+    public void OpenShop()
+    {
+        mainPanel.SetActive(false);
+        shop.SetActive(true);
+        //buildingSelected = true;
+    }
+
+    public void BuyBuilding(int number)
+    {
+        int buildingNumber = number;
+        shop.SetActive(false);
+        mainPanel.SetActive(true);
+        GameObject house = Instantiate(structureArray[buildingNumber], spawnPointOfBuildings.transform.position, Quaternion.identity);
+        //selectedBuilding = house;
+        Building buildingScript = house.GetComponent<Building>();
+        buildingScript.buildingState = Building.BuildingState.firstStart;
+        buildingScript.mouseDrag = true;
+        buildingSelected = true;
+    }
+
+    public void GameOver(bool win)
+    {
+        youWinLabel.SetActive(win);
+        youLostLabel.SetActive(!win);
+        gameOver.SetActive(true);
+    }
+}
