22b587d56471d29c1e33401ce7f2c2aa9188fb48
kaletejas <tejaskale.messi@gmail.com>
2023-06-16 15:15:46 -0400

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+/*Author: Tejas Kale
+ * Student Number: 301237417
+ * File: Pause Menu
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+   // public GameObject mainMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+       // mainMenuUI.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quiting ..");  
+    }
+}
